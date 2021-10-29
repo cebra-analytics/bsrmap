b584fe187283ad233b7310c9bfc1ad866411d870
seanhaythorne <seanhaythorne@hotmail.com>
2021-10-30 10:11:38 +1100

    Renamed calc_EE to calc_establish_event_pr and calc_pathway_pr to calc_pathway_establish_pr to avoid name clashing with edmaps. Also some variable name and doc changes.

--- R/calc_establish_event_pr.R
+++ R/calc_establish_event_pr.R
@@ -1,7 +1,7 @@
 #' Calculate establishment event probabilities
 #'
-#' Calculate (via stochastic sampling) the probability of one or more 
-#' establishment events occurring as as a function of estimated leakage numbers 
+#' Calculate (via stochastic sampling) the probability of one or more
+#' establishment events occurring as as a function of estimated leakage numbers
 #' and the probability a leakage event could result in establishment.
 #'
 #' @param n_events Numeric vector containing lower and upper bounds (CI) for
@@ -12,28 +12,28 @@
 #' @param n_sims Integer. Number of samples to be taken from event and
 #'   establishment distributions. Default = 100000.
 #' @param ... Additional parameters (unused).
-#' @return A \code{data.frame} containing the possible number of incursions 
+#' @return A \code{data.frame} containing the possible number of incursions
 #'   that may occur, and their corresponding probabilities.
 #' @references Camac, J. & Baumgartner, J. (2021). \emph{edmaps} (early
-#'   detection maps) : An R package for creating Australian maps of 
+#'   detection maps) : An R package for creating Australian maps of
 #'   establishment likelihood for terrestrial plant pests.
 #'   \url{https://github.com/jscamac/edmaps}.
-#' @note Modified version of 
+#' @note Modified version of
 #'   \code{\href{edmaps}{https://github.com/jscamac/edmaps}::calc_EE}.
 #' @export
-calc_EE <- function(n_events, p_establish, 
-                    confidence = 0.95,
-                    n_sims = 100000, ...) {
-  
+calc_establish_event_pr <- function(n_events, p_establish,
+                                    confidence = 0.95,
+                                    n_sims = 100000, ...) {
+
   # Check n_events and p_establish confidence intervals
   if (is.numeric(n_events) && length(n_events) != 2 &&
       n_events[1] < n_events[2]) {
-    stop("n_events (CI) should have numeric form: (lower, upper).", 
+    stop("n_events (CI) should have numeric form: (lower, upper).",
          call. = FALSE)
   }
   if (is.numeric(p_establish) && length(p_establish) != 2 &&
       p_establish[1] < p_establish[2]) {
-    stop("p_establish (CI) should have numeric form: (lower, upper).", 
+    stop("p_establish (CI) should have numeric form: (lower, upper).",
          call. = FALSE)
   }
 
@@ -41,25 +41,25 @@ calc_EE <- function(n_events, p_establish,
   log_mean <- mean(log(n_events))
   log_sd <- ((log_mean - log(min(n_events)))/
                stats::qnorm(1 - (1 - confidence)/2))
-  
+
   # Sample lambda from lognormal
   lambda <- stats::rlnorm(n = n_sims, meanlog = log_mean, sdlog = log_sd)
-  
+
   # Sample leakage from poisson using lambda
   n <- stats::rpois(n = n_sims, lambda = lambda)
-  
+
   ## Calculate establishment probability
   logit_mean <- mean(stats::qlogis(p_establish))
   logit_sd <- ((logit_mean - stats::qlogis(min(p_establish)))/
                  stats::qnorm(1 - (1 - confidence)/2))
-  
+
   # Sample probability from logit normal
   logit_p <- stats::rnorm(n = n_sims, mean = logit_mean, sd = logit_sd)
-  
+
   # Sample incursion events from binomial distribution
   est <- stats::rbinom(n = n_sims, size = n, prob = stats::plogis(logit_p))
   est <- table(est)/length(est)
-  
+
   # Return a table of probabilities for incursion numbers
   return(data.frame(N_incursions = as.integer(names(est)),
                     probability = c(est)))
--- R/pathway_likelihood.R
+++ R/pathway_likelihood.R
@@ -1,12 +1,12 @@
-#' Estimate pathway pest arrival likelihood
+#' Estimate pathway pest establishment likelihood
 #'
-#' Estimates spatially-explicit pest arrival likelihoods for a given pathway
-#' from overall leakage and establishment rate estimates and their likely
-#' spatial distribution.
+#' Estimates spatially-explicit pest establishment likelihoods for a given
+#' pathway from overall leakage and establishment rate estimates and their
+#' likely spatial distribution.
 #'
 #' @param pathway_layers A \code{raster::Raster*} or \code{terra::SpatRaster}
 #'   object containing one or more layers for estimating the spatial
-#'   distribution of arrival likelihood.
+#'   distribution of establishment likelihood.
 #' @param leakage_rate_ci Numeric vector of 2 values, giving the lower and
 #'   upper bounds (CI) for leakage rate (the number of pest leakage events in
 #'   a random year).
@@ -17,16 +17,16 @@
 #' @param filename Optional file writing path (character).
 #' @param ... Additional parameters (passed to \code{writeRaster}).
 #' @return A \code{raster::RasterLayer} or \code{terra::SpatRaster} object
-#'   (as per \code{pathway_layers}) containing estimated pathway pest arrival
-#'   likelihoods.
+#'   (as per \code{pathway_layers}) containing estimated pathway pest
+#'   establishment likelihoods.
 #' @references Camac, J. & Baumgartner, J. (2021). \emph{edmaps} (early
 #'   detection maps) : An R package for creating Australian maps of
 #'   establishment likelihood for terrestrial plant pests.
 #'   \url{https://github.com/jscamac/edmaps}.
 #' @note Generalized modified version of
 #'   \code{\href{edmaps}{https://github.com/jscamac/edmaps}::arrivals_by_*}.
-#' @include calc_EE.R
-#' @include calc_pathway_pr.R
+#' @include calc_establish_event_pr.R
+#' @include calc_pathway_establish_pr.R
 #' @include combine_layers.R
 #' @export
 pathway_likelihood <- function(pathway_layers,
@@ -81,11 +81,12 @@ pathway_likelihood.SpatRaster <- function(pathway_layers,
   combined_layer <- combined_layer/as.numeric(
     terra::global(combined_layer, fun = "sum", na.rm = TRUE))
 
-  # Calculate establishment event probabilities
-  establ_event_probs <- calc_EE(leakage_rate_ci, establishment_rate_ci,
-                                confidence)
+  # Calculate calc_pathway_establish_pr event probabilities
+  establ_event_probs <- calc_establish_event_pr(leakage_rate_ci,
+                                                establishment_rate_ci,
+                                                confidence)
 
   # Distribute probabilities across combined layer
-  return(calc_pathway_pr(combined_layer, establ_event_probs,
-                         filename = filename, ...))
+  return(calc_pathway_establish_pr(combined_layer, establ_event_probs,
+                                   filename = filename, ...))
 }
