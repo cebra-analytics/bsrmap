0ab87036c4b76263fac6471e5dddd40bdd32b54c
seanhaythorne <seanhaythorne@hotmail.com>
2022-08-04 17:10:03 +1000

    abiotic/biotic suitability tests

--- tests/testthat/test_abiotic_suitability.R
+++ tests/testthat/test_abiotic_suitability.R
@@ -1,5 +1,16 @@
 context("abiotic_suitability")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("conforms layer to template", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  abiotic_rast <- terra::rast(file.path(TEST_DIRECTORY, "abiotic.tif"))
+  template_rast <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))*0
+  expect_silent(new_layer <- suppressMessages(
+    abiotic_suitability(abiotic_rast, template_rast)))
+  expect_true(terra::crs(new_layer) == terra::crs(template_rast))
+  expect_true(terra::ext(new_layer) == terra::ext(template_rast))
+  expect_true(all(terra::res(new_layer) == terra::res(template_rast)))
+  expect_true(round(mean(new_layer[], na.rm = TRUE), 2) ==
+                round(mean(terra::crop(abiotic_rast,
+                                       c(144.5, 145.5, -38.5, -37.5))[],
+                           na.rm = TRUE), 2))
 })
--- tests/testthat/test_biotic_suitability.R
+++ tests/testthat/test_biotic_suitability.R
@@ -1,5 +1,11 @@
 context("biotic_suitability")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("combines layers via product by default", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  popn_rast <- terra::rast(file.path(TEST_DIRECTORY, "population.tif"))
+  veg_rast <- terra::rast(file.path(TEST_DIRECTORY, "vegetation.tif"))
+  stack_rast <- terra::rast(list(popn_rast, veg_rast))
+  expect_silent(comb_rast <- suppressMessages(
+    biotic_suitability(stack_rast)))
+  expect_equal(comb_rast[][,1], popn_rast[][,1]*veg_rast[][,1])
 })
