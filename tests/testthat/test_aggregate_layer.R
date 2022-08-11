d8588590abbc8c5aab4751e0354294aaa60a23a7
seanhaythorne <seanhaythorne@hotmail.com>
2022-08-11 16:46:49 +1000

    aggregate_layer tests

--- tests/testthat/test_aggregate_layer.R
+++ tests/testthat/test_aggregate_layer.R
@@ -1,5 +1,21 @@
 context("aggregate_layer")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("aggregates and re-samples layer to match template", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  aclum_rast <- terra::rast(file.path(TEST_DIRECTORY, "aclum.tif"))
+  cropped_ext <- terra::ext(aclum_rast) - c(0, 5000, 0, 5000)
+  template_rast <- terra::crop(terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))*0,
+                               cropped_ext)
+  expect_silent(aggr_rast <- suppressMessages(
+    aggregate_layer(aclum_rast, template_rast, use_fun = "max")))
+  expect_true(terra::ext(aggr_rast) == terra::ext(template_rast))
+  expect_true(terra::crs(aggr_rast) == terra::crs(template_rast))
+  expect_true(all(terra::res(aggr_rast) == terra::res(template_rast)))
+  value_matrix <- matrix(terra::crop(aclum_rast, cropped_ext)[],
+                         nrow = 100, byrow = TRUE)
+  expected_values <- array(sapply(as.data.frame(
+    t(expand.grid(0:4, 0:4))), function(a) {
+      max(value_matrix[(1:20) + a[1]*20, (1:20) + a[2]*20])
+    }), c(5, 5))
+  expect_true(all(aggr_rast[][,1] == t(expected_values)))
 })
