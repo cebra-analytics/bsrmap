ab61002057cad977b77b111a9c3cfcc9dfcf4fa4
seanhaythorne <seanhaythorne@hotmail.com>
2022-08-12 14:36:49 +1000

    buffered_hull_layer tests

--- tests/testthat/test_buffered_hull_layer.R
+++ tests/testthat/test_buffered_hull_layer.R
@@ -1,5 +1,31 @@
 context("buffered_hull_layer")
 
-test_that("test 1", {
-  expect_true(TRUE)
+test_that("builds alpha or convex hull layer via set of points", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  template_rast <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))*0
+  expect_error(buffered_hull_layer(template_rast, data.frame(a = 1:4)),
+               "Point (features) y data should have at least 2 columns.",
+               fixed = TRUE)
+  expect_error(buffered_hull_layer(template_rast,
+                                   data.frame(a = 1:4, b = 3:6)),
+               paste("Point (features) y data coordinates should be 'lon'",
+                     "and 'lat'."), fixed = TRUE)
+  example_pts <- data.frame(lon = c(144.3, 144.3, 144.4, 144.5, 144.5),
+                            lat = c(-38.0, -37.6, -37.8, -37.9, -37.8))
+  expect_error(buffered_hull_layer(template_rast, example_pts),
+               "Fitting an alpha hull requires a numeric 'alpha' parameter.")
+  expect_silent(new_layer1 <- suppressMessages(
+    buffered_hull_layer(template_rast, example_pts, alpha = 100000)))
+  expect_silent(new_layer2 <- suppressMessages(
+    buffered_hull_layer(template_rast, example_pts, hull = "convex")))
+  expect_silent(new_layer3 <- suppressMessages(
+    buffered_hull_layer(template_rast, example_pts, hull = "convex",
+                        buffer = 10000)))
+  idx_1 <- which(new_layer1[][,1] > 0)
+  idx_2 <- which(new_layer2[][,1] > 0)
+  idx_3 <- which(new_layer3[][,1] > 0)
+  expect_true(all(idx_1 %in% idx_3))
+  expect_true(all(idx_2 %in% idx_3))
+  expect_true(length(idx_1) < length(idx_2))
+  expect_true(length(idx_2) < length(idx_3))
 })
