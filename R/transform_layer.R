5a6791cc7a350a7fdee7584987f22b30dee8a1b0
seanhaythorne <seanhaythorne@hotmail.com>
2023-02-08 14:27:09 +1100

    v0.1.1 added transform layer function

---
+++ R/transform_layer.R
@@ -0,0 +1,113 @@
+#' Transform spatial layer
+#'
+#' Transforms a spatial layer via a variety of operations, including the
+#' application of: linear, exponential, or logarithmic expressions; or lower
+#' or upper thresholds; to layer values.
+#'
+#' @param x A \code{raster::RasterLayer} or \code{terra::SpatRaster}
+#'   object representing the spatial layer to be transformed.
+#' @param type One of \code{"linear"} (\code{a*x + b}), \code{"exponential"}
+#'   (\code{a^x*x^b}), \code{"logarithmic"} (\code{log(x, base = b)}),
+#'   \code{"lower"} (\code{x[x < a] = b}), or \code{"upper"}
+#'   (\code{x[x > a] = b}).
+#' @param a Numeric parameter for transformation as specified by \code{type}.
+#'   Default (\code{NULL}) implies: \code{1} for \code{"linear"},
+#'   \code{"exponential"}, and \code{"upper"}; and \code{0} for \code{"lower"}.
+#' @param b Numeric parameter for transformation as specified by \code{type}.
+#'   Default (\code{NULL}) implies: \code{0} for \code{"linear"},
+#'   \code{"exponential"}, and \code{"lower"}; \code{1} for \code{"upper"}; and
+#'   \code{exp(1)} (natural) for \code{"logarithmic"}.
+#' @param filename Optional file writing path (character).
+#' @param ... Additional parameters (passed to \code{writeRaster}).
+#' @return A \code{terra::SpatRaster} object containing the transformed layer.
+#' @export
+transform_layer <- function(x,
+                            type = c("linear",
+                                     "exponential",
+                                     "logarithmic",
+                                     "lower",
+                                     "upper"),
+                            a = NULL,
+                            b = NULL,
+                            filename = "", ...) {
+  UseMethod("transform_layer")
+}
+
+#' @name transform_layer
+#' @export
+transform_layer.Raster <- function(x,
+                                   type = c("linear",
+                                            "exponential",
+                                            "logarithmic",
+                                            "lower",
+                                            "upper"),
+                                   a = NULL,
+                                   b = NULL,
+                                   filename = "", ...) {
+
+  # Call the terra version of the function
+  transform_layer(terra::rast(x),
+                  type = type,
+                  a = a,
+                  b = b,
+                  filename = filename, ...)
+}
+
+#' @name transform_layer
+#' @export
+transform_layer.SpatRaster <- function(x,
+                                       type = c("linear",
+                                                "exponential",
+                                                "logarithmic",
+                                                "lower",
+                                                "upper"),
+                                       a = NULL,
+                                       b = NULL,
+                                       filename = "", ...) {
+
+  # Match arguments for type
+  type <- match.arg(type)
+
+  # Resolve/check a and b
+  if (is.null(a)) {
+    if (type %in% c("lower")) {
+      a <- 0
+    } else if (type %in% c("linear", "exponential", "upper")) {
+      a <- 1
+    }
+  } else if (!is.numeric(a) || length(a) > 1) {
+    stop("Parameter 'a' should be a single value numeric.", call. = FALSE)
+  }
+  if (is.null(b)) {
+    if (type %in% c("linear", "exponential", "lower")) {
+      b <- 0
+    } else if (type %in% c("upper")) {
+      b <- 1
+    } else if (type %in% c("logarithmic")) {
+      b <- exp(1)
+    }
+  } else if (!is.numeric(b) || length(b) > 1) {
+    stop("Parameter 'b' should be a single value numeric.", call. = FALSE)
+  }
+
+  # Perform transformation
+  if (type == "linear") {
+    x <- a*x + b
+  } else if (type == "exponential") {
+    x <- a^x*x^b
+  } else if (type == "logarithmic") {
+    x <- log(x, base = b)
+  } else if (type == "lower") {
+    x <- (x < a)*b + (x >= a)*x
+  } else if (type == "upper") {
+    x <- (x > a)*b + (x <= a)*x
+  }
+  names(x) <- sprintf("%s_trans", type)
+
+  # Write to file when required
+  if (is.character(filename) && nchar(filename) > 0) {
+    x <- terra::writeRaster(x, filename, ...)
+  }
+
+  return(x)
+}
---
+++ tests/testthat/test_transform_layer.R
@@ -0,0 +1,45 @@
+context("transform_layer")
+
+test_that("various transformations", {
+  TEST_DIRECTORY <- test_path("test_inputs")
+  suit_rast <- terra::rast(file.path(TEST_DIRECTORY, "greater_melb.tif"))
+  idx <- which(!is.na(suit_rast[]))
+  suit_values <- suit_rast[idx][,1]
+  expect_error(transform_layer(suit_rast, a = "1"),
+               "Parameter 'a' should be a single value numeric.")
+  expect_error(transform_layer(suit_rast, b = 1:2),
+               "Parameter 'b' should be a single value numeric.")
+  expect_silent(trans_rast <- transform_layer(suit_rast))
+  expect_is(trans_rast, "SpatRaster")
+  expect_equal(trans_rast[idx][,1], suit_values)
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "linear",
+                                              a = 2, b = 3))
+  expect_equal(trans_rast[idx][,1], 2*suit_values + 3)
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "exponential"))
+  expect_equal(trans_rast[idx][,1], suit_values*0 + 1)
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "exponential",
+                                              a = 2))
+  expect_equal(trans_rast[idx][,1], 2^suit_values)
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "exponential",
+                                              b = 3))
+  expect_equal(trans_rast[idx][,1], suit_values^3)
+  expect_silent(trans_rast <- transform_layer(suit_rast + 1,
+                                              type = "logarithmic"))
+  expect_equal(trans_rast[idx][,1], log(suit_values + 1))
+  expect_silent(trans_rast <- transform_layer(suit_rast + 1,
+                                              type = "logarithmic", b = 10))
+  expect_equal(trans_rast[idx][,1], log10(suit_values + 1))
+  expect_silent(trans_rast <- transform_layer(suit_rast - 0.1, type = "lower"))
+  expect_equal(trans_rast[idx][,1], (suit_values >= 0.1)*(suit_values - 0.1))
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "lower",
+                                              a = 0.2, b = 0.1))
+  expect_equal(trans_rast[idx][,1],
+               (suit_values < 0.2)*0.1 + (suit_values >= 0.2)*suit_values)
+  expect_silent(trans_rast <- transform_layer(suit_rast*2, type = "upper"))
+  expect_equal(trans_rast[idx][,1],
+               (suit_values > 0.5)*1 + (suit_values <= 0.5)*suit_values*2)
+  expect_silent(trans_rast <- transform_layer(suit_rast, type = "upper",
+                                              a = 0.5, b = 0.5))
+  expect_equal(trans_rast[idx][,1],
+               (suit_values > 0.5)*0.5 + (suit_values <= 0.5)*suit_values)
+})
